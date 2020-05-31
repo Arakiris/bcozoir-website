c0a1c46850f1eb93231afabeb37d27c88f2cda68
François VONGVILAY <f.vongvilay@gmailc.om>
2020-05-31 02:09:07 +0200

    Fix audio

--- resources/views/layouts/partials/_main.blade.php
+++ resources/views/layouts/partials/_main.blade.php
@@ -1,5 +1,9 @@
 @include('layouts/partials/_leftsidebar')
 <section class="content">
+    <audio class="content__audio" id="audio" autoplay loop >
+        <source src="{{ isset($music_link->path) ? asset('storage' . $music_link->path) : asset('music/music.mp3') }}" type="audio/mpeg">
+            <p>Votre navigateur ne prend pas en charge l'audio HTML.</p>
+    </audio>
     @if(isset($warnings) && $warnings->count()>0)
         <div class="content__warning warning-carousel warning">
             @foreach($warnings as $warning)
